57de62639d38140fb9a80649c61aa795cbced756
so_young <qksthdhd94@gmail.com>
2019-11-04 19:54:27 +0900

    화면 이동 - startActivityForResult 취소 버튼 - finish 화면 종료.

--- app/src/main/java/com/example/a20191104_01_intent/EditUserInfoActivity.kt
+++ app/src/main/java/com/example/a20191104_01_intent/EditUserInfoActivity.kt
@@ -2,11 +2,16 @@ package com.example.a20191104_01_intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_user_info.*
 
 class EditUserInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_user_info)
+
+        cancleBtn.setOnClickListener {
+            finish()
+        }
     }
 }
