bc58c4c725cef4a437289b91cca5e50b0b7dfc65
so_young <qksthdhd94@gmail.com>
2019-11-04 21:07:10 +0900

    폰번 입력 요청 화면 그리기

--- app/src/main/java/com/example/a20191104_01_intent/MainActivity.kt
+++ app/src/main/java/com/example/a20191104_01_intent/MainActivity.kt
@@ -8,6 +8,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var REQ_CODE_FOR_NAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         nameInputBtn.setOnClickListener {
 
             var intent = Intent(this,EditUserInfoActivity::class.java)
-            startActivityForResult(intent,1000)
+
+            if(nameEdt.text.toString() != "이름 입력 필요"){
+                intent.putExtra("userName",nameEdt.text.toString())
+            }
+
+            startActivityForResult(intent,REQ_CODE_FOR_NAME)
 
 
         }
@@ -25,14 +32,17 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == 1000){
+        if(requestCode == REQ_CODE_FOR_NAME){
             if(resultCode == RESULT_OK){
 
-                var inputNameData = data?.getStringExtra("inputName")
+                var inputNameData = data?.getStringExtra("inputName")?.toLowerCase()
 
                 inputNameData?.let {
 
-                    nameTxt.text = it                }
+                    nameTxt.text = it
+
+                    var myName:String? = null
+                }
 
             }
         }
